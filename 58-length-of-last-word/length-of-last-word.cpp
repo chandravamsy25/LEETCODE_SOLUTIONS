4910d4126b56dce840f01752f065edc3140bd805
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2023-10-14 11:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (6.97%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int k=s.length();
+        int count=0;
+        for(int i =k-1;i<s.length();i--)
+        {
+            
+           
+            if(s[i]==' ' && count>=1 ) break;
+            
+            if(s[i]!=' ') count++;
+            
+
+        }
+        
+        return count;
+    }
+};
