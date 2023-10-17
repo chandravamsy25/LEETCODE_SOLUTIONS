860376e1987866881f10419da6df8c34ec6d3d9d
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2023-10-17 11:39:34 +0530

    Time: 42 ms (52.89%) | Memory: 32.8 MB (73.25%) - LeetSync

---
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+         vector<int> parentFreq(n);
+        for (int i = 0; i < n; i++)
+            if ((leftChild[i] >= 0 and ++parentFreq[leftChild[i]] > 1) or (rightChild[i] >= 0 and ++parentFreq[rightChild[i]] > 1))
+                return false;
+       
+        int root = -1;
+        for (int i = 0; i < n; i++)
+            if (!parentFreq[i]) {
+                if (root == -1)
+                    root = i;
+                else return false;
+            }
+        
+        return countNodes(leftChild, rightChild, root) == n;
+    }
+    
+    int countNodes(vector<int> &left, vector<int> &right, int root) {
+        if (root == -1)
+            return 0;
+        return 1 + countNodes(left, right, left[root]) + countNodes(left, right, right[root]);
+        
+    }
+};
