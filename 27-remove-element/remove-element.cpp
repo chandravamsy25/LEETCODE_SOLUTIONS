6b30b09ccaaa44fd31d90ac2694314e9a67ea425
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2023-10-25 20:31:43 +0530

    Time: 4 ms (30.94%) | Memory: 9.1 MB (23.65%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> arr;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=val)
+            {
+                arr.emplace_back(nums[i]);
+            }
+        }
+        
+        nums=arr;
+        
+
+        return nums.size();
+        
+    }
+};
