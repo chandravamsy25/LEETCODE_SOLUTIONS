c0a955a13eec5c3c28e47700ac78291f8ab41766
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2023-11-02 15:08:49 +0530

    Time: 8 ms (87.92%) | Memory: 16.2 MB (10.34%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
@@ -6,22 +6,13 @@ public:
         for(int i=1;i<nums.size();i++)
         {
             if(nums[i]<=nums[i-1])
-            {
-                
-                if (nums[i-1]>nums[i])
-                {
-                    
-                    count =count +(nums[i-1]-nums[i])+1;
-                    nums[i]=nums[i-1]+1;
-
-                } 
-                
-                else count++;
-                nums[i]=nums[i-1]+1;
+            {     
+                count =count +(nums[i-1]-nums[i])+1;
+                nums[i]=nums[i-1]+1;    
                 
             }
         }
-        cout<<nums.size();
+        
         return count ;
         
     }
