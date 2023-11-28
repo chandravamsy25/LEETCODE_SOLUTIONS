a212730a0140c5dc252237bf431d8e4d85e1f166
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2023-11-28 17:40:28 +0530

    Time: 117 ms (82.29%) | Memory: 31 MB (76.57%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-    const long long mod=1e9+7;
-    int dp[100001][3];
-    int recur(string &cor,int idx,int seats)
-    {
-        //Base Cases
-        if(seats>2)
-            return 0;
-        
-        //Limiting Case
-        if(idx>=cor.size())
-        {
-            return seats==2;
-            
-        }
-        
-        // Memoization 
-        if(dp[idx][seats]!=-1)
-            return dp[idx][seats]%mod;
-        
-        
-        // I don't Partition Here and move forward
-        int op1=recur(cor,idx+1,seats+(cor[idx]=='S'));
-        
-        
-        // I partition only if my Seat count is exactly = 2 and
-        // add the result to previous count
-        if(seats==2)
-            op1=(op1+(recur(cor,idx,0)))%mod;
-        
-        
-        return dp[idx][seats]=op1%mod;
-        
-        
-        
-    }
     int numberOfWays(string corridor) {
-        
-        memset(dp,-1,sizeof dp);
-        return recur(corridor,0,0)%mod;
+        int mod = pow(10,9) + 7;
+        long long ans = 1;
+        int temp = 0;
+        int count = 0;
+        for(int i = 0; i < corridor.length(); i++){
+            if(count != 2){
+                if(corridor[i]=='S'){
+                    count++;
+                }
+            }
+            else{
+                if(corridor[i]=='S'){
+                    temp++;
+                    ans *= temp;
+                    ans %= mod;
+                    count = 1;
+                    temp = 0;
+                }
+                else{
+                    temp++;
+                }
+            }
+
+        }
+        if(count != 2){
+            return 0;
+        }
+        else{
+            return ans;
+        }
     }
 };
