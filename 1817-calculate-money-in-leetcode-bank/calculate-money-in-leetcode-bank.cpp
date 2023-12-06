d9de64f6dcc370de849b1764ae41cd9912cc9b1a
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2023-12-06 22:16:07 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (35.53%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int mon=1,sum=0,k=1;
+        for(int i=1;i<=n;i++){
+            sum+=k;
+            k++;
+            if((i)%7==0){
+                k=mon+1;
+                mon++;
+            }
+        }return sum;
+    }
+};
