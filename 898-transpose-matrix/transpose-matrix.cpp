9be6b8ff0e0b294cf4c62e5617b8eafe25143b38
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2023-12-10 09:36:47 +0530

    Time: 10 ms (52.49%) | Memory: 10.8 MB (84.60%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.cpp
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -4,7 +4,7 @@ public:
         
         int row=matrix.size();
         int col=matrix[0].size();
-        vector<vector<int>> mat(col,vector<int>(row,0));
+        vector<vector<int>> mat(col,vector<int>(row));
         for(int i=0;i<col;i++)
         {
             for(int j=0;j<row;j++)
