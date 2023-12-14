6f1e2694cd89659345a3f5c2c07011e0c9157ae9
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2023-12-14 10:35:16 +0530

    Time: 181 ms (87.98%) | Memory: 117.4 MB (76.35%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,47 +1,34 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
         
-        int m=grid.size();
-        int n=grid[0].size();
-        
-        vector<int> row(m,0);
-        vector<int> column(n,0);
+        vector<int> row(m, 0);
+        vector<int> column(n, 0);
         vector<vector<int>> diff(m, vector<int>(n, 0));
     
-
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==0) row[i]--;
-                if(grid[i][j]==1) row[i]++;    
-            
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 0) {
+                    row[i]--;
+                    column[j]--;
+                } else if(grid[i][j] == 1) {
+                    row[i]++;
+                    column[j]++;
+                }
             }
-            
         }
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[j][i]==0) column[i]--;
-                if(grid[j][i]==1) column[i]++;    
-            
-            }
-            
-        }
-
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                diff[i][j]=row[i]+column[j];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                diff[i][j] = row[i] + column[j];
             }
         }
 
         return diff;
-        
-        
     }
 };
