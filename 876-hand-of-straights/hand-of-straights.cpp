d97c86517468ef2f7f51491bb7abedd429772a08
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2024-06-06 22:35:18 +0530

    Time: 65 ms (40.94%) | Memory: 30.6 MB (54.60%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if(hand.size() % groupSize != 0)return false;
+        map<int,int> umap;
+        for(int i=0; i<hand.size(); i++){
+            umap[hand[i]]++;
+        }
+        int start = 0;
+        int curr;
+        while(umap.size()>0){
+            curr = umap.begin()->first;
+            for(int i=0; i<groupSize; i++){
+                if(umap[curr+i] == 0)return false;
+                --umap[curr+i];
+                if(umap[curr+i] == 0)umap.erase(curr+i);
+            }
+        }
+        return true;
+    }
+};
