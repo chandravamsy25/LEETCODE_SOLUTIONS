d978773c486bdbf67ade14d9f91df5c73b6480da
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2024-06-11 14:34:18 +0530

    Time: 3 ms (53.89%) | Memory: 9.2 MB (98.03%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int>arr3;
+        vector<int>arr4;
+        
+        int count;
+        sort(arr3.begin(),arr3.end());
+        vector<bool>freq(false);
+       
+        for(int i=0;i<arr1.size();i++)
+        {
+            count=0;
+            for(int j=0;j<arr2.size();j++)
+            {
+                if(arr1[i]==arr2[j]) count++;
+     
+            }
+            if(count==0) arr4.emplace_back(arr1[i]);
+        }
+        
+        
+        for(int i=0;i<arr2.size();i++)
+        {
+            for(int j=0;j<arr1.size();j++){
+                if(arr1[j]==arr2[i]){
+                    arr3.emplace_back(arr1[j]);
+                    
+                }
+            }
+        }
+        sort(arr4.begin(),arr4.end());
+        for(int i=0;i<arr4.size();i++){
+            arr3.emplace_back(arr4[i]);
+        }
+        return arr3;
+        
+        
+    }
+};
