853a8d4e61fa8af229a02cdfa4d0189913838b65
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2024-06-25 10:42:56 +0530

    Time: 4 ms (55.92%) | Memory: 14.6 MB (54.31%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -15,10 +15,12 @@ public:
 
         if(ind==-1){
             reverse(nums.begin(),nums.end());
+            return;
             
         }
-        else{
-            for(int i=n-1;i>ind;i--){
+        
+        
+        for(int i=n-1;i>ind;i--){
             if(nums[i]>nums[ind]){
                 swap(nums[i],nums[ind]);
                 break;
@@ -26,9 +28,6 @@ public:
         }
         
         reverse(nums.begin()+ind+1,nums.end());
-        }
-        
-        
         
         
     }
