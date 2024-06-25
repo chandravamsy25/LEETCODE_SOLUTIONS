6d779cedcb9e2c3c3d68dc5c064e86c5bef978ad
chandravamsy25 <90548992+chandravamsy25@users.noreply.github.com>
2024-06-25 10:41:44 +0530

    Time: 8 ms (14.68%) | Memory: 14.4 MB (86.97%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int ind=-1;
+        int n=nums.size();
+        // finding breakeven index
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;  
+            }
+        }
+
+        //edge case
+
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+            
+        }
+        else{
+            for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+        
+        reverse(nums.begin()+ind+1,nums.end());
+        }
+        
+        
         
         
     }
